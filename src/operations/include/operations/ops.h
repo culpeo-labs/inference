af38488dcb4799bb95c4308481558d34f385698d
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2026-08-09 01:09:48 +0000

    gate simd behind def

--- src/operations/include/operations/ops.h
+++ src/operations/include/operations/ops.h
@@ -46,84 +46,129 @@ namespace culpeo::inference::operations {
         }
     }
 
-    inline float horizontal(const __m256 val)
+    namespace details
     {
-        /* lower: val[0:127] (f1, f2, f3, f4), cast (free) */
-        auto lower = _mm256_castps256_ps128(val);
-        /* upper: val[128, 255] (f5, f6, f7, f8), extract (instruction) */
-        auto upper = _mm256_extractf128_ps(val, 1);
-        /* lower: (f1 + f5, f2 + f6, f3 + f7, f4 + f8)*/
-        lower = _mm_add_ps(lower, upper);
-        /* hadd(a, b) -> (a1 + a2, a3 + a4, b1 + b2, b3 + b4) */
-        /* lower: (l1 + l2, l3 + l4, _, _)*/
-        lower = _mm_hadd_ps(lower, lower);
-        /* lower: (l1 + l2 + l3 + l3, _, _, _) */
-        lower = _mm_hadd_ps(lower, lower);
-        return _mm_cvtss_f32(lower);
-    }
+        enum class _dot_impl_tag
+        {
+            scalar,
+            AVX2,
+        };
 
-    template<typename T>
-    inline __m256 load(const T * ptr)
-    {
-        if constexpr (sizeof(T) == 2)
+        template<_dot_impl_tag impl = _dot_impl_tag::scalar>
+        struct dot_impl
         {
-            /* T == int16_t (bfloat16) */
-            __m128i i128 = _mm_loadu_si128(reinterpret_cast<const __m128i *>(ptr));
-            auto i256 = _mm256_cvtepu16_epi32(i128);
-            /* shift, bf16 -> f32 */
-            i256 = _mm256_slli_epi32(i256, 16);
-            return _mm256_castsi256_ps(i256);
-        }
-        else
+            static float operator()(util::float_vector auto a, util::float_vector auto b)
+            {
+                float acc{ 0 };
+                for (std::size_t c = 0; c < a.extent(0); c++)
+                {
+                    acc += a[c] * b[c];
+                }
+                return acc;
+            }
+        };
+
+
+        template<>
+        struct dot_impl<_dot_impl_tag::AVX2>
         {
-            /* T == float */
-            return _mm256_loadu_ps(ptr);
+            static inline float horizontal(const __m256 val)
+            {
+                /* lower: val[0:127] (f1, f2, f3, f4), cast (free) */
+                auto lower = _mm256_castps256_ps128(val);
+                /* upper: val[128, 255] (f5, f6, f7, f8), extract (instruction) */
+                auto upper = _mm256_extractf128_ps(val, 1);
+                /* lower: (f1 + f5, f2 + f6, f3 + f7, f4 + f8)*/
+                lower = _mm_add_ps(lower, upper);
+                /* hadd(a, b) -> (a1 + a2, a3 + a4, b1 + b2, b3 + b4) */
+                /* lower: (l1 + l2, l3 + l4, _, _)*/
+                lower = _mm_hadd_ps(lower, lower);
+                /* lower: (l1 + l2 + l3 + l3, _, _, _) */
+                lower = _mm_hadd_ps(lower, lower);
+                return _mm_cvtss_f32(lower);
+            }
+
+            template<typename T>
+            static inline __m256 load(const T * ptr)
+            {
+                if constexpr (sizeof(T) == 2)
+                {
+                    /* T == int16_t (bfloat16) */
+                    __m128i i128 = _mm_loadu_si128(reinterpret_cast<const __m128i *>(ptr));
+                    auto i256 = _mm256_cvtepu16_epi32(i128);
+                    /* shift, bf16 -> f32 */
+                    i256 = _mm256_slli_epi32(i256, 16);
+                    return _mm256_castsi256_ps(i256);
+                }
+                else
+                {
+                    /* T == float */
+                    return _mm256_loadu_ps(ptr);
+                }
+            }
+
+            static float operator()(util::float_vector auto a, util::float_vector auto b)
+            {
+                std::array<__m256, 4> accs;
+                util::unroll<4>([&](auto i)
+                {
+                    accs[i] = _mm256_setzero_ps();
+                });
+
+                const auto end = a.extent(0) - a.extent(0) % 32;
+
+                for (auto c : std::views::iota(std::size_t{0 }, end) | std::views::stride(32))
+                {
+                    util::unroll<4>([&](auto i)
+                    {
+                        accs[i] = _mm256_fmadd_ps(load(
+                            a.data_handle() + c + i * 8
+                        ), load(b.data_handle() + c + i * 8), accs[i]);
+                    });
+                }
+
+                accs[0] = _mm256_add_ps(accs[0], accs[1]);
+                accs[2] = _mm256_add_ps(accs[2], accs[3]);
+                accs[0] = _mm256_add_ps(accs[0], accs[2]);
+                auto total = horizontal(accs[0]);
+                if ((a.extent(0) % 32) > 0)
+                {
+                    for (auto c : std::views::iota(end, a.extent(0)))
+                    {
+                        total += a[c] * b[c];
+                    }
+                }
+                return total;
+
+            }
+        };
+
+        template<_dot_impl_tag dot_impl>
+        void matvec(util::mat_t<float, 1> out, util::float_matrix auto W, util::float_vector auto x)
+        {
+            static util::function_timer timer{ std::source_location::current() };
+            auto _ = timer.probe();
+            assert(out.extent(0) == W.extent(0));
+            assert(x.extent(0) == W.extent(1));
+            assert(W.stride(0) == W.extent(1));
+            for (std::size_t r = 0; r < W.extent(0); r++)
+            {
+                auto w_row = util::get_row(W, r);
+
+                out[r] = details::dot_impl<dot_impl>{}(w_row, x);
+            }
         }
     }
 
     void matvec(util::mat_t<float, 1> out, util::float_matrix auto W, util::float_vector auto x)
     {
-        static util::function_timer timer{ std::source_location::current() };
-        auto _ = timer.probe();
-        assert(out.extent(0) == W.extent(0));
-        assert(x.extent(0) == W.extent(1));
-        assert(W.stride(0) == W.extent(1));
-        for (std::size_t r = 0; r < W.extent(0); r++)
-        {
-            auto w_row = util::get_row(W, r);
-            std::array<__m256, 4> accs;
-            util::unroll<4>([&](auto i)
-            {
-                accs[i] = _mm256_setzero_ps();
-            });
-
-            const auto end = w_row.extent(0) - w_row.extent(0) % 32;
-
-            for (auto c : std::views::iota(std::size_t{0 }, end) | std::views::stride(32))
-            {
-                util::unroll<4>([&](auto i)
-                {
-                    accs[i] = _mm256_fmadd_ps(load(
-                        w_row.data_handle() + c + i * 8
-                    ), load(x.data_handle() + c + i * 8), accs[i]);
-                });
-            }
-
-            accs[0] = _mm256_add_ps(accs[0], accs[1]);
-            accs[2] = _mm256_add_ps(accs[2], accs[3]);
-            accs[0] = _mm256_add_ps(accs[0], accs[2]);
-            auto total = horizontal(accs[0]);
-            if ((w_row.extent(0) % 32) > 0)
-            {
-                for (auto c : std::views::iota(end, w_row.extent(0)))
-                {
-                    total += w_row[c] * x[c];
-                }
-            }
-
-            out[r] = total;
-        }
+    #ifdef  USE_SIMD
+        details::matvec<details::_dot_impl_tag::AVX2>(out, W, x);
+    #else
+        details::matvec<details::_dot_impl_tag::scalar>(out, W, x);
+    #endif
     }
+
 
     void rope(util::mat_t<float, 2> t, std::ptrdiff_t pos, float theta_base);
 
